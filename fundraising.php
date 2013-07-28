4b5e9689e0e11fa44c9a7885f9fcf902a8e93a86
meyerhoffl <laura.meyerhoff@gmail.com>
2013-07-28 13:57:14 -0500

    fundraising page mockup

--- fundraising.php
+++ fundraising.php
@@ -1,34 +1,49 @@
-<?php get_header ();
+<?php
 /*
 Template Name: Fundraising Page
 */
 ?>
-
 <?php get_header(); ?>
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/CSS/communityStyle.css">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/CSS/fundraisingStyle.css">
 
-<style type='text/css'>
-  .backgroundImgLt {
-    background-image: url('<?php echo get_template_directory_uri(); ?>/images/first_aid_kit.png');
-  }
-
-  @-moz-document url-prefix() {
-    .titleSize {
-      margin-top:0px;
-      padding-top: 5px;
-    }
-
-</style>
-
-<div id="backgroundWrapper" class="clearfix">
-	<div id="communityWrapper" class="clearfix">
-		<div class="sectionWrap clearfix">
-			<section class="clearfix">
-      	<h3 class="emphasisTitle aboutSmall">Fundraising</h3>
-      </section>
+<div class="contentWrap">
+  <div class="accordionNav">
+    <a href="#">Capital Campaign</a>
+    <a href="#">Operating Costs Campaign</a>
+  </div>
+  <h2 class="smallHeadline">Contribute</h2>
+  <h3 class="emphasisTitle">Invest in Nashville's Tech Community</h3>
+  <div class="left">
+    <p>
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+    </p>
+    <p>
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+    </p>
+    <p>
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+      Text about how great NSS is and why to contribute.
+    </p>
+  </div>
+  <div class="right">
+    <div class="image_placeholder">
+      <p>Image/Video</p>
     </div>
   </div>
 </div>
-
-
 <?php get_footer(); ?>
