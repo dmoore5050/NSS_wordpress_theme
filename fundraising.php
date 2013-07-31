0b2f897b70e4553c8e7d62305b4916c69e779bde
dmoore5050 <dustinmoore@gmail.com>
2013-07-31 15:02:37 -0500

    changes to support page style

--- fundraising.php
+++ fundraising.php
@@ -4,49 +4,40 @@ Template Name: Fundraising Page
 */
 ?>
 <?php get_header(); ?>
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/CSS/fundraisingMockupStyle.css">
+<link rel='stylesheet' type='text/css' href='<?php echo get_template_directory_uri(); ?>/CSS/fundraisingMockupStyle.css'>
+<div class='container clearfix'>
+  <div class='contentWrap'>
+    <h2 id='introHeadline'>Contribute:</h2>
+    <h3 id='mainSupportHead'>Invest in Nashville's Tech Community</h3>
+    <div id='navWrap'>
+      <div class='subNav'>
+          <a href='/NSS/support/capitalcampaign' class='subNavItem'>Capital Campaign</a>
+          <a href='/NSS/support/campaign' class='subNavItem'>Operating Costs Campaign</a>
+      </div>
+    </div>
+    <div class='left'>
+      <p>
+        Text about how great NSS is and why to contribute.
+        Text about how great NSS is and why to contribute.
+        Text about how great NSS is and why to contribute.
+        Text about how great NSS is and why to contribute.
+        Text about how great NSS is and why to contribute.
+        Text about how great NSS is and why to contribute.
+      </p>
 
-<div class="contentWrap">
-  <div class="accordionNav">
-    <div class='navItemWrapper'>
-      <a href="/NSS/support/capitalcampaign">Capital Campaign</a>
+      <p>
+        Text about how great NSS is and why to contribute.
+        Text about how great NSS is and why to contribute.
+        Text about how great NSS is and why to contribute.
+        Text about how great NSS is and why to contribute.
+        Text about how great NSS is and why to contribute.
+        Text about how great NSS is and why to contribute.
+      </p>
     </div>
-    <div class='navItemWrapper'>
-      <a href="/NSS/support/campaign">Operating Costs Campaign</a>
-    </div>
-  </div>
-  <h2 class="smallHeadline">Contribute</h2>
-  <h3 class="emphasisTitle">Invest in Nashville's Tech Community</h3>
-  <div class="left">
-    <p>
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-    </p>
-    <p>
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-    </p>
-    <p>
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-      Text about how great NSS is and why to contribute.
-    </p>
-  </div>
-  <div class="right">
-    <div class="image_placeholder">
-      <p>Image/Video</p>
+    <div class='right'>
+      <div class='video_placeholder'>
+        Video Placeholder
+      </div>
     </div>
   </div>
 </div>
