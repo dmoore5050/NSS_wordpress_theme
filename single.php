46268f439d672096a87d12484d95bdd12e51a8c0
dmoore5050 <dustinmoore@gmail.com>
2013-08-08 14:27:06 -0500

    switched single post template from tabs to spaces

--- single.php
+++ single.php
@@ -6,13 +6,13 @@
  */
 get_header(); ?>
 <div class="contentWrap">
-	<?php while ( have_posts() ) : the_post(); ?>
-		<section class='meta'>
-			<p><?php the_author();?> <?php the_time('F j, Y');?></p>
-		</section>
-		<h1><?php the_title();?></h1>
-		<?php the_content();?>
-		<?php comments_template( '', true ); ?>
-	<?php endwhile;?>
+  <?php while ( have_posts() ) : the_post(); ?>
+    <section class='meta'>
+      <p><?php the_author();?> <?php the_time('F j, Y');?></p>
+    </section>
+    <h1><?php the_title();?></h1>
+    <?php the_content();?>
+    <?php comments_template( '', true ); ?>
+  <?php endwhile;?>
 </div>
 <?php get_footer(); ?>
