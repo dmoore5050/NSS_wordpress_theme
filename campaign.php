f130f5524c56def6336e8c9dbaf59a849180242f
meyerhoffl <laura.meyerhoff@gmail.com>
2013-08-21 09:53:14 -0500

    added copy

--- campaign.php
+++ campaign.php
@@ -91,33 +91,69 @@ Template Name: Campaign Page
   <div id='contentWrapper' class='clearfix'>
     <div id='mainWrapper'>
       <div id='campaignContentWrapper'>
-        <section id='campaignMedia'>
-          <iframe width='620' height='349' src='//www.youtube.com/embed/8AdSzQZ8AKo?rel=0' frameborder='0' allowfullscreen></iframe>
-        </section>
         <section id='campaignBody'>
-          <h2 class='bodyTitle'>Body Title Here</h2>
-          <div id='leadWrap'>
-            <p id='leadParagraph'>"Etsy Echo Park literally sriracha. Wolf cred hashtag, next level blue bottle four loko occupy. Disrupt mumblecore try-hard, cliche biodiesel moustache salvia pickled whatever."<span id="leadName"> -John Wark</span></p>
-          </div>
-          <p class='bodyText'>Small batch typewriter fanny pack, yr vinyl readymade salvia biodiesel chillwave selfies tumblr pour-over. Actually blog letterpress, art party sriracha shabby chic selvage Echo Park Carles. Fap wolf +1 next level, butcher fanny pack hoodie Brooklyn salvia dreamcatcher mumblecore. 8-bit lo-fi polaroid selfies plaid post-ironic. Truffaut High Life fashion axe, cred banjo keytar 3 wolf moon food truck butcher McSweeney's Williamsburg chillwave gluten-free photo booth. </p>
+          <h2 class='bodyTitle'>Sustain Nashville Software School</h2>
+          <p id='leadParagraph'>We need your support to be able to continue to grow Nashville's tech talent pool.</p>
+            <section id='campaignMedia'>
+              <iframe width='576' height='349' src='//www.youtube.com/embed/8AdSzQZ8AKo?rel=0' frameborder='0' allowfullscreen></iframe>
+            </section>
+            <p class='bodyText'>
+              All of us who have been working in and around the Nashville tech community, or who have been trying to hire or partner with skilled software developers, have known for years that Nashville has a painful shortage of these critical resources.  We've all seen the quarterly surveys from the Nashville Technology Council that show a consistent 800 to 1100 open tech jobs in Nashville.  We've all shared the frustration of spending months trying to fill software developer positions.
+              In 2012 a group of Nashville technologists got together to try to find a way to address this problem.  We believed that Nashville had plenty of people with the latent talent to become tech professionals.  We knew that it didn't take a four year college degree and computer science degree to become a productive professional software developer because many of us didn't have such a degree.  We believed that we could partner with Nashville area technology employers to create a curriculum that would create the skills they needed in an entry-level software developer.
+            <p class='bodyText'>The result?</p>
+            <h2 class="bodyTitle">Nashville Software School</h2>
+            </p>
           <p class='bodyText'>
-            <img class='bodyImage' src='<?php bloginfo('template_url'); ?>/images/codeschooltopper.jpg' alt='filler image' width='320px;' height='320px;'/>Pitchfork labore locavore artisan exercitation, incididunt hoodie odio banjo Godard roof party Etsy. Mollit adipisicing cray aliqua. Qui officia lo-fi fugiat you probably haven't heard of them Tonx. Sed tofu cillum duis Terry Richardson, organic leggings food truck american apparel asymmetrical mumblecore.</p>
-          <p class='bodyText'>Meggings banh mi forage flannel Marfa, ethnic mumblecore Odd Future freegan 90's Neutra wolf. Bitters McSweeney's irony you probably haven't heard of them, helvetica church-key cliche. Fixie DIY occupy, pitchfork asymmetrical retro keffiyeh freegan stumptown viral PBR tattooed Etsy pug. Master cleanse forage sustainable ethical, Marfa street art. Echo Park Carles. Fap wolf +1 next level, butcher fanny pack hoodie Brooklyn salvia dreamcatcher mumblecore. 8-bit lo-fi polaroid selfies plaid post-ironic. </p>
-          <br>
-          <h2 class='bodyTitle right'>Another Body Title Here</h2>
-          <aside class='right smallAside'>
-            <a href='#' id='modalTrigger' data-reveal-id='videoModal' data-animation='fade' data-animationspeed='300'>
-              <div id='fakePic'></div>
-            </a>
-            <div id='videoModal' class='reveal-modal hidden' >
-              <iframe id='videoFrame' width='620' height='349' src='//www.youtube.com/embed/8AdSzQZ8AKo?rel=0' frameborder='0' allowfullscreen></iframe>
-            </div>
-            <h3 id='asideTitle'>Johnny Fakename</h3>
-            <p id='asideText'>Mumblecore. 8-bit lo-fi polaroid selfies plaid post-ironic. Fixie DIY occupy, pitch fork asymmetrical retro keffiyeh freegan stump town viral PBR tattoo Etsy pug bahn mi.</p>
-          </aside>
-          <p class='bodyText'>Bushwick artisan deep v ethnic, pop-up cardigan freegan gluten-free single-origin coffee Portland. Ullamco pitchfork pour-over PBR, thundercats hoodie Bushwick hella roof party reprehenderit readymade wolf aliqua. Meggings viral fap magna, blue bottle quis ad Austin. Messenger bag assumenda accusamus yr bitters, deep v vegan VHS magna culpa ennui small batch. Exercitation before they sold out.</p>
-          <p class='bodyText'>Master cleanse iPhone lo-fi Bushwick. Freegan mixtape laborum, occaecat photo booth officia Brooklyn voluptate squid iPhone id roof party. Meggings banh mi forage flannel Marfa, ethnic mumblecore Odd Future freegan 90's Neutra wolf. Bitters McSweeney's irony you probably haven't heard of them, helvetica church-key cliche. Bitters McSweeney's irony you probably haven't heard of them, helvetica church-key cliche assumenda hoodie nostrud 8-bit. Odd Future freegan 90's Neutra wolf. Bitters McSweeney's irony you probably haven't heard of them, helvetica church-key cliche. </p>
-          <br>
+            Nashville Software School (NSS) is a career accelerator for highly motivated adult learners who possess the aptitude to become software developers.  We offer a six month, full-time vocational training program that creates career-ready entry-level software developers.
+          </p>
+          <p class='bodyText'>
+            We are a strongly community-centered organization.  Our founders, instructors and mentors are all working technology professionals in the Nashville area who have donated time, dollars and energy to help prove that the NSS model works.
+          </p>
+          <p class='bodyText'>
+            We are also a Tennessee non-profit corporation.  Our mission is to open doors to technology careers to qualified applicants irrespective of their financial resources and to grow the local tech workforce for the benefit of all local startups or established organizations.
+          </p>
+          <h2 class="bodyTitle">
+            So does this really work?
+          </h2>
+          <p class='bodyText'>
+            We have graduated 40 junior software developers since starting our first class in June 2012.  In late 2012 we graduated our first cohort of 14 developers and at mid-year 2013 we graduated an additional 26.  Most of the graduates of our first cohort are employed in software developer or other technology jobs around Nashville.  Several in our second cohort have already secured full-time positions and the remainder are actively searching for positions in Nashville.
+          </p>
+          <p class='bodyText'>
+            Our alumni work at local companies including Centresource, OnLife Health, Checkd.in, Change Healthcare, ioStudio, Zeumo, Eliza Brock Software, LiveSchool, and BorderJump.
+          </p>
+          <p class='bodyText'>
+            We have also proven to our satisfaction that there is no shortage of demand in Nashville for training from people seeking careers in software development.  And there is no shortage of people with the latent potential to become professional developers seeking a way to activate that potential.  We stopped taking applications for our third cohort of students at 200 applications - of whom at least 50 were fully qualified for admission to NSS.
+          </p>
+          <h2 class="bodyTitle">
+            NSS invests in our students
+          </h2>
+          <p class='bodyText'>
+            As noted above, NSS is open to qualified applicants irrespective of their ability to pay for six months of full-time training.  In practical terms this means that we only ask our students for a nominal commitment fee at the beginning of the program - currently $1000.  Our cost to train a student is significantly greater than $1000.  But rather than require significant tuition payments up-front, payments that would have created significant barriers to a tech career for many of the great students that have graduated from our program, we recoup our investment in our students at the end of our program when they go to work as professional developers or in other technology jobs.
+          </p>
+          <p class='bodyText'>
+            This approach aligns our incentives with those of our students.  We are incented to only admit those who we truly believe have the motivation and aptitude to succeed as professional developers.  We are incented to ensure that they are learning both the foundational knowledge required of all professional developers but that they are also learning tools and technologies that are widely used.  We are also incented to ensure our students become deeply connected with the Nashville tech community where they aspire to work.
+          </p>
+          <p class='bodyText'>
+            We then recoup our investment once our students graduate and go to work.  Our partner companies that hire graduates pay us a placement fee to offset our investment in the student and that relieves the student of his obligation to the school.  Or, for those students who don't go to work for partner companies, the students repay us from their earnings as a working tech professional.
+          </p>
+          <h2 class="bodyTitle">
+            What do we need the money for?
+          </h2>
+          <p class='bodyText'>
+            We love the business model we have designed but it means that we spend a lot training a class of graduates before we begin to recoup that investment through placement fees and repayments.  That means we need to maintain a working capital reserve large enough to sustain the schoolÕs operation through a full training and repayment cycle.
+          </p>
+          <p class='bodyText'>
+            We have been able to successfully bootstrap NSS for its first year of operation.  The financial support of our founders and advisory board, plus the placement fees and repayments from our first class have allowed us to demonstrate the ability to train and place our graduates into software developer jobs.  However, to reach full sustainability, we now need the support of the full Nashville community to help us build the endowment - our working capital reserve -  that we need to fully fund the school.
+          </p>
+          <p class='bodyText'>
+            We have a third cohort of student starting the program in September 2013.  That will be our last cohort of students if we can't engage the broader support of the Nashville community.
+          </p>
+          <p class='bodyText'>
+            Help us build our capital reserves.  Help us ensure that we will be able to continue to open doors to careers in technology for motivated students.  Help us continue to grow Nashville's technology talent pool by training hungry, committed junior developers.  Help ensure that when the time comes that you are looking for developers to hire for your team, when you are looking for a freelancer to build your website or mobile app, when you are searching for a technical co-founder for your startup that those resources will be available to you right here in Nashville.
+          </p>
+          <p class='bodyText'>
+            Support us now by selecting a donation level from the options in the column to the right.  Help sustain Nashville Software School.
+          </p>
           <?php $wp_query = new WP_Query("post_type=campaign_update&tag=capital&cat=22&post_status=publish&posts_per_page=1");?>
           <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
           <?php the_content ();?>
